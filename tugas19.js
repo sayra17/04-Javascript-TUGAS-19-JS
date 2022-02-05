5bea7c845a3399e207db1d0402c84a963dd87409
sayra17 <agussayra03@gmail.com>
2022-02-05 16:52:07 +0700

    Initial commit

---
+++ tugas19.js
@@ -0,0 +1,19 @@
+function ulang() {
+    var kata = "semangat..."
+    console.log(kata.repeat(5))
+}
+ulang()
+function ulangbintang() {
+    var bintang = "*"
+    console.log(bintang.repeat(5))
+    console.log(bintang.repeat(20))
+    console.log(bintang.repeat(35))
+    console.log(bintang.repeat(50))
+    console.log(bintang.repeat(60))
+    console.log(bintang.repeat(50))
+    console.log(bintang.repeat(35))
+    console.log(bintang.repeat(20))
+    console.log(bintang.repeat(5))
+
+}
+ulangbintang()
